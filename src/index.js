ba6c08256cb1307f2f99e63b92b4f8c3598ba101
Mattias Wikström <mw222rs@student.lnu.se>
2015-03-30 00:50:09 +0200

    getting into react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,34 @@
 
 var React = require('react');
 
-React.render(
-	<h1>Hello yo!</h1>,
-	document.body
-);
+var App = React.createClass({
+	getInitialState:function(){
+		return {
+			txt: ""
+		}
+	},
+	update:function(e){
+		this.setState({txt: e.target.value});
+	},
+	render:function(){
+		return (
+			<div>
+				<Widget txt={this.state.txt} update={this.update} />
+			</div>
+			);
+	}
+});
+
+var Widget = React.createClass({
+	render:function(){
+		return (
+			<div>
+				<input type="text" onChange={this.props.update} />
+				<br />
+				<h1>{this.props.txt}</h1>
+			</div>
+			);
+	}
+});
+
+React.render(<App txt="this is the txt prop" />, document.body)
