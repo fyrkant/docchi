fcce47efaf2fb2b81c921fd2a23b6aba7f209c8b
Mattias Wikström <mw222rs@student.lnu.se>
2015-04-30 20:44:07 +0200

    somethings just change, you know

--- src/components/write.js
+++ src/components/write.js
@@ -1,30 +1,32 @@
-var React = require('react');
-/*	Reflux = require('reflux'),
-	Firebase = require('firebase'),
-	_ = require('lodash'),
-	$ = require('jquery'),
-	ReactFireMixin = require('reactfire'),
-	TodoList = require('./todolist'),
-	actions = require('../actions'),
-	WriteStore = require('../writestore')*/
+// var React = require('react'),
+// 	Reflux = require('reflux'),
+// 	Firebase = require('firebase'),
+// 	_ = require('lodash'),
+// 	$ = require('jquery'),
+// 	ReactFireMixin = require('reactfire'),
+// 	TodoList = require('./todolist'),
+// 	actions = require('../actions'),
+// 	WriteStore = require('../writestore');
 
-//var myFirebase = new Firebase("https://blazing-fire-8429.firebaseio.com/docchi/");
+// var myFirebase = new Firebase("https://blazing-fire-8429.firebaseio.com/docchi/");
 
 
 
-var TodoApp = React.createClass({
-	render: function() {
-		return (		<div className="col-sm-6">
-				<h3>WRITE</h3>
-				<div className="col-sm-8">
-				    <p className="text-muted">Här ska man skriva historier!</p>
-				</div>
-			</div>
-		);
-	},
-	componentWillUnmount: function() {
-	}
+// var TodoApp = React.createClass({
+// 	mixins:[ReactFireMixin, Reflux.connect(WriteStore)],
+// 	getInitialState()
+// 	render() {
+// 		return (		<div className="col-sm-6">
+// 				<h3>WRITE</h3>
+// 				<div className="col-sm-8">
+// 				    <p className="text-muted">Här ska man skriva historier!</p>
+// 				</div>
+// 			</div>
+// 		);
+// 	},
+// 	componentWillUnmount: function() {
+// 	}
 
-});
+// });
 
-module.exports = TodoApp;
+// module.exports = TodoApp;
